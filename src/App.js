38581a75d74eaa975ea0c25233f00c6898f3cfc4
Veekas Shrivastava <veekas@veekasmeansprogress.com>
2018-04-23 06:52:23 -0700

    refactor(App): remove React logo and styles

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 
 import Planet from './Planet';
 
-import logo from './logo.svg';
 import './App.css';
 import { getPlanets } from './api';
 
@@ -21,7 +20,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Planets of Star Wars</h1>
         </header>
         <ul className="App-intro">
