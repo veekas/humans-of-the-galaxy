95f71e9437c6dd70d740175b3f6e9ba4fa4fbb23
Veekas Shrivastava <veekas@veekasmeansprogress.com>
2018-04-24 16:53:52 -0400

    refactor(ResidentList): change empty resident list message

--- src/ResidentList.js
+++ src/ResidentList.js
@@ -5,7 +5,7 @@ import { ListItem } from './styled/listElements';
 const ResidentList = ({ residents }) => (
   residents.length
     ? residents.map(resident => <ListItem key={resident.name}>{resident.name}</ListItem>)
-    : <ListItem>none</ListItem>
+    : <ListItem>No one lives here.</ListItem>
 );
 
 export default ResidentList;
