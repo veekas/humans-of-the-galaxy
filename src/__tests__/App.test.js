5736d771d39d3d4908fc5cdbd43759f86f582170
Veekas Shrivastava <veekas@veekasmeansprogress.com>
2018-04-24 23:59:28 -0400

    test(App): add snapshot  test

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { create } from 'react-test-renderer';
 import App from '../App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('<App>', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('matches snapshot on shallow render', () => {
+    const component = create(<App />);
+    const snap = component.toJSON();
+    expect(snap).toMatchSnapshot();
+  });
 });
