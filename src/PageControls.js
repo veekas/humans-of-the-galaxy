b1b392143e537ee072e6d6477b8321ea75c01ff3
Veekas Shrivastava <veekas@veekasmeansprogress.com>
2018-04-25 03:15:32 -0400

    fix(PageControls): remove extra space

--- src/PageControls.js
+++ src/PageControls.js
@@ -5,7 +5,7 @@ import { Title, Control, ControlsContainer } from './styled/appElements';
 const PageControls = ({ pageNumber, paginate }) => (
   <ControlsContainer>
     <Control src="back.svg" onClick={() => paginate('back')} />
-    <Title> Page {pageNumber} of 7</Title>
+    <Title>Page {pageNumber} of 7</Title>
     <Control src="next.svg" onClick={() => paginate('next')} />
   </ControlsContainer>
 );
